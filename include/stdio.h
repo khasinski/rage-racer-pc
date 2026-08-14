868cfbafee22b0e07eb9b5d8014e71d0f0d1673d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-14 12:33:29 +0200

    Expose printf to Windows game sources

--- include/stdio.h
+++ include/stdio.h
@@ -5,6 +5,8 @@
 
 #ifdef _WIN32
 typedef struct _iobuf FILE;
+int printf(const char *format, ...);
+int sprintf(char *dest, const char *format, ...);
 FILE *fopen(const char *path, const char *mode);
 char *fgets(char *buffer, int size, FILE *stream);
 int fclose(FILE *stream);
