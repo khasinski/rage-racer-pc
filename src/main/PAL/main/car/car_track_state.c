76b961e35e242d739ec802386d658e84793c70bd
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-01 14:28:47 +0200

    Separate car surface and progress calculations

--- src/main/PAL/main/car/car_track_state.c
+++ src/main/PAL/main/car/car_track_state.c
@@ -136,27 +136,98 @@ static void PlaceCarOnArc(GameCarRuntime *obj, CarTrackWork *work,
     }
 }
 
+static s32 InterpolateTrackValue(s32 start, s32 end, s32 alongSegment,
+                                 s16 segmentLength) {
+    return (end * alongSegment + start * (segmentLength - alongSegment)) /
+           segmentLength;
+}
+
+/* The original fixed-point maths rounds negative products towards zero. */
+static s32 ShiftFixed12(s32 value) {
+    if (value < 0) {
+        value += 0xFFF;
+    }
+    return value >> 12;
+}
+
+static s32 ProjectTrackAxis(s32 value) {
+    if (value < 0) {
+        value += 0xFFF;
+    }
+    return value >> 14;
+}
+
+static void UpdateCarSurfaceOrientation(GameCarRuntime *obj,
+                                        CarTrackWork *work,
+                                        const GameTrackPoint *point,
+                                        const GameTrackPoint *nextPoint,
+                                        s32 alongSegment,
+                                        s32 lateralOffset) {
+    s16 segmentLength = (s16)work->segmentLength;
+    s16 trackWidth;
+    s32 nextCamber;
+    s32 pointCamber;
+    s32 surfaceHeight;
+
+    work->crossSlope = (s16)InterpolateTrackValue(
+        point->crossSlope, nextPoint->crossSlope, alongSegment, segmentLength);
+    surfaceHeight = InterpolateTrackValue(
+        point->y, nextPoint->y, alongSegment, segmentLength);
+    obj->y = ((work->crossSlope * lateralOffset) >> 7) + surfaceHeight;
+
+    work->relativeHeading = (s16)((u16)obj->bodyYaw - 0xC00 +
+                                  (u16)work->heading);
+    work->surfacePitch = (s16)InterpolateTrackValue(
+        point->surfacePitch, nextPoint->surfacePitch, alongSegment,
+        segmentLength);
+
+    trackWidth = (u16)work->rightHalfWidth + (u16)work->leftHalfWidth;
+    work->trackWidth = trackWidth;
+    nextCamber = Atan2(trackWidth,
+                       (nextPoint->crossSlope * trackWidth) >> 7);
+    pointCamber = Atan2(work->trackWidth,
+                        (point->crossSlope * work->trackWidth) >> 7);
+    work->camberAngle = (s16)InterpolateTrackValue(
+        pointCamber, nextCamber, alongSegment, segmentLength);
+
+    work->headingCos = rcos(work->relativeHeading);
+    work->headingSin = rsin(work->relativeHeading);
+    obj->bodyPitch = ShiftFixed12(work->surfacePitch * work->headingCos) +
+                     ShiftFixed12(work->camberAngle * work->headingSin);
+    obj->bodyRoll = ShiftFixed12(-work->headingCos * work->camberAngle) +
+                    ShiftFixed12(work->surfacePitch * work->headingSin);
+}
+
+static void UpdateCarTrackProgress(GameCarRuntime *obj, CarTrackWork *work,
+                                   s32 alongSegment, s32 lateralOffset) {
+    s32 lapProgress;
+    s32 sectionProgress;
+
+    obj->trackLateralOffset = lateralOffset;
+    obj->progressB = g_RaceSeries != 0
+        ? (u32)alongSegment
+        : (u32)((s16)work->segmentLength - alongSegment);
+
+    lapProgress = (obj->progressA + obj->progressB) % g_TrackLength;
+    obj->trackHeading.value = work->heading;
+    obj->previousTrackProgress = obj->trackProgress;
+    obj->trackProgress = lapProgress < 0
+        ? lapProgress + g_TrackLength
+        : lapProgress;
+
+    sectionProgress = g_RaceSeries != 0
+        ? g_TrackLength - obj->trackProgress
+        : obj->trackProgress;
+    obj->trackSection = (s16)(sectionProgress >> 8);
+}
+
 s32 UpdateCarTrackState(GameCarRuntime *obj, s32 trackPointIndex, CarTrackLimits *limits) {
     s16 curveMode;
     s32 headingAngle;
-    s32 secondResult;
-    s16 segLenE;
-    s16 trackWidth;
-    s16 trackWidthCopy;
-    s16 segLenC;
-    s16 segLenD;
-    s16 segLenF;
-    s32 trackLength;
-    s32 forwardComponent;
-    s32 lapProgress;
     s32 nextPointIndex;
-    s32 surfaceHeight;
     s32 cosHeading;
     s32 sinHeading;
-    s32 nextCamber;
     s32 arcIndex;
-    s32 forwardProduct;
-    s32 lateralProduct;
     s32 lateralOffset;
     s32 alongSegment;
     s32 rotated;
@@ -192,14 +263,13 @@ s32 UpdateCarTrackState(GameCarRuntime *obj, s32 trackPointIndex, CarTrackLimits
     }
 
     nextPointIndex = (trackPointIndex + 1) % g_TrackPointCount;
-    work = (&g_CarTrackWork);
+    work = &g_CarTrackWork;
     work->knockbackMode = 0;
     point = TrackPoint(trackPointIndex);
     segmentLength = point->segmentLength;
     work->segmentLength = segmentLength;
     nextPoint = TrackPoint(nextPointIndex);
-    if ((s16)segmentLength <= 0)
-    {
+    if ((s16)segmentLength <= 0) {
         work->segmentLength = 1U;
     }
     work->heading = (u16)point->angle;
@@ -216,142 +286,33 @@ s32 UpdateCarTrackState(GameCarRuntime *obj, s32 trackPointIndex, CarTrackLimits
     work->offsetZ = (s16)(((u16)obj->z - (u16)point->z) * 4);
     work->offsetY = 0;
     cosHeading = rcos(headingAngle);
-    rotated = (cosHeading * (s16) work->offsetX) + (rsin(work->heading) * work->offsetZ);
-    if (rotated < 0)
-    {
-        rotated += 0xFFF;
-    }
-    alongSegment = rotated >> 0xE;
+    rotated = (cosHeading * (s16)work->offsetX) +
+              (rsin(work->heading) * work->offsetZ);
+    alongSegment = ProjectTrackAxis(rotated);
     sinHeading = rsin(work->heading);
-    rotated = ((0 - sinHeading) * (s16) work->offsetX) + (rcos(work->heading) * work->offsetZ);
-    if (rotated < 0)
-    {
-        rotated += 0xFFF;
-    }
-    lateralOffset = rotated >> 0xE;
-    if (work->curveMode != 0)
-    {
+    rotated = (-sinHeading * (s16)work->offsetX) +
+              (rcos(work->heading) * work->offsetZ);
+    lateralOffset = ProjectTrackAxis(rotated);
+    if (work->curveMode != 0) {
         lateralOffset = work->arcLateral;
     }
     lateralOffset = ClampCarToTrackEdges(obj, work, limits, point,
                                          nextPoint, alongSegment,
                                          lateralOffset);
-    if ((s16)work->segmentLength < alongSegment)
-    {
+    if ((s16)work->segmentLength < alongSegment) {
         alongSegment = (s16)work->segmentLength;
-    }
-    else if (alongSegment < 0)
-    {
+    } else if (alongSegment < 0) {
         alongSegment = 0;
     }
     obj->segmentFraction = (alongSegment << 0xA) / (s16)work->segmentLength;
-    if (lateralOffset < 0)
-    {
+    if (lateralOffset < 0) {
         obj->normalizedLateralOffset = (lateralOffset * 0x400) / work->leftHalfWidth;
-    }
-    else
-    {
+    } else {
         obj->normalizedLateralOffset = (lateralOffset * 0x400) / work->rightHalfWidth;
     }
-    {
-        u32 outputProgress;
-        s32 useProgress;
-
-        useProgress = g_RaceSeries;
-        obj->trackLateralOffset = lateralOffset;
-        if (useProgress != 0)
-        {
-            outputProgress = alongSegment;
-        }
-        else
-        {
-            outputProgress = (s16)work->segmentLength - alongSegment;
-        }
-        obj->progressB = outputProgress;
-    }
-    segLenC = (s16)work->segmentLength;
-    work->crossSlope = (s16)((nextPoint->crossSlope * alongSegment +
-                           point->crossSlope * (segLenC - alongSegment)) /
-                          segLenC);
-    segLenD = (s16)work->segmentLength;
-    surfaceHeight =
-        (nextPoint->y * alongSegment + point->y * (segLenD - alongSegment)) / segLenD;
-    obj->y = ((work->crossSlope * lateralOffset) >> 7) + surfaceHeight;
-    {
-        s16 angle;
-
-        angle = (u16)obj->bodyYaw;
-        angle -= 0xC00;
-        work->relativeHeading = angle + (u16)work->heading;
-    }
-    segLenE = (s16)work->segmentLength;
-    work->surfacePitch = (s16)((nextPoint->surfacePitch * alongSegment +
-                           point->surfacePitch * (segLenE - alongSegment)) /
-                          segLenE);
-    trackWidth = (u16) work->rightHalfWidth + (u16) work->leftHalfWidth;
-    work->trackWidth = trackWidth;
-    nextCamber = Atan2(trackWidth, (nextPoint->crossSlope * trackWidth) >> 7);
-    trackWidthCopy = work->trackWidth;
-    secondResult = Atan2(trackWidthCopy, (point->crossSlope * trackWidthCopy) >> 7);
-    segLenF = (s16)work->segmentLength;
-    work->camberAngle =
-        (s16)((nextCamber * alongSegment + secondResult * (segLenF - alongSegment)) / segLenF);
-    work->headingCos = rcos(work->relativeHeading);
-    {
-        s32 firstProduct;
-        s32 sinValue;
-        s32 secondProduct;
-
-        sinValue = rsin(work->relativeHeading);
-        work->headingSin = sinValue;
-        firstProduct = work->surfacePitch * work->headingCos;
-        if (firstProduct < 0)
-        {
-            firstProduct += 0xFFF;
-        }
-        firstProduct >>= 0xC;
-        secondProduct = work->camberAngle * sinValue;
-        if (secondProduct < 0)
-        {
-            secondProduct += 0xFFF;
-        }
-        obj->bodyPitch = firstProduct + (secondProduct >> 0xC);
-    }
-    forwardProduct = (0 - work->headingCos) * work->camberAngle;
-    if (forwardProduct < 0)
-    {
-        forwardProduct += 0xFFF;
-    }
-    lateralProduct = work->surfacePitch * work->headingSin;
-    forwardComponent = forwardProduct >> 0xC;
-    if (lateralProduct < 0)
-    {
-        lateralProduct += 0xFFF;
-    }
-    trackLength = g_TrackLength;
-    lapProgress = (obj->progressA + obj->progressB) % trackLength;
-    obj->bodyRoll = forwardComponent + (lateralProduct >> 0xC);
-    obj->trackHeading.value = work->heading;
-    obj->previousTrackProgress = obj->trackProgress;
-    obj->trackProgress = lapProgress;
-    if (lapProgress < 0)
-    {
-        obj->trackProgress = lapProgress + trackLength;
-    }
-    {
-        s32 finalAngle;
-
-        if (g_RaceSeries != 0)
-        {
-            finalAngle = g_TrackLength - obj->trackProgress;
-            obj->trackSection = (s16)(finalAngle >> 8);
-        }
-        else
-        {
-            finalAngle = obj->trackProgress;
-            obj->trackSection = (s16)(finalAngle >> 8);
-        }
-    }
+    UpdateCarSurfaceOrientation(obj, work, point, nextPoint, alongSegment,
+                                lateralOffset);
+    UpdateCarTrackProgress(obj, work, alongSegment, lateralOffset);
     if (traceThisCall) {
         Trace("car-track-exit", "timer=%d point=%d x=%d z=%d progress=%d "
                "lateral=%d along=%d heading=%d curve=%d widths=%d,%d "
