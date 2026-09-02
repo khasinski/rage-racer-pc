3e00db2dcc00b02771cbfaa1edff8b93465dcd00
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-02 21:12:38 +0200

    Separate rival jump phases

--- src/main/PAL/main/car/rival_body_motion.c
+++ src/main/PAL/main/car/rival_body_motion.c
@@ -1,41 +1,62 @@
 #include "game/car.h"
 #include "game/car_internal.h"
 
+static void UpdateRivalRise(GameCarRuntime *car, s32 ground, s32 tick) {
+    s32 rise = (s16)tick;
+
+    car->y += car->verticalMotionRate * rise +
+              rise * rise * CAR_JUMP_RISE_CURVE / CAR_JUMP_CURVE_SCALE;
+    if (car->y >= ground) {
+        car->verticalMotionState = CAR_VERTICAL_GROUNDED;
+    }
+}
+
+static void UpdateRivalCrest(GameCarRuntime *car, s32 ground) {
+    if (car->verticalTargetY >= ground - car->verticalMotionRate) {
+        car->y = car->verticalTargetY;
+    } else {
+        car->verticalMotionState = CAR_VERTICAL_FALLING;
+        car->verticalMotionRate = car->verticalMotionTimer;
+        car->y = car->verticalTargetY;
+    }
+}
+
+static void UpdateRivalFall(GameCarRuntime *car, s32 ground, s32 tick) {
+    s16 fall = tick - (u16)car->verticalMotionRate;
+
+    car->y = car->verticalTargetY +
+             fall * fall * CAR_JUMP_FALL_CURVE / CAR_JUMP_CURVE_SCALE;
+    if (car->y >= ground) {
+        car->verticalMotionState = CAR_VERTICAL_GROUNDED;
+    }
+}
+
+static void LandRivalCar(GameCarRuntime *car, s32 ground) {
+    car->y = ground + CAR_WHEEL_GROUND_OFFSET;
+    car->verticalPitch = 0;
+    car->verticalRoll = 0;
+    StartCarBodyKick(car, CAR_BODY_KICK_LANDING);
+}
+
 static void UpdateRivalJumpArc(GameCarRuntime *car, s32 ground) {
     s32 tick = (u16)car->verticalMotionTimer + 1;
 
     car->verticalMotionTimer = tick;
-    if (car->verticalMotionState == CAR_VERTICAL_RISING) {
-        s32 rise = (s16)tick;
-
-        car->y += car->verticalMotionRate * rise +
-                  rise * rise * CAR_JUMP_RISE_CURVE / CAR_JUMP_CURVE_SCALE;
-        if (car->y >= ground) {
-            car->verticalMotionState = CAR_VERTICAL_GROUNDED;
-        }
-    } else if (car->verticalMotionState == CAR_VERTICAL_AT_CREST) {
-        if (car->verticalTargetY >= ground - car->verticalMotionRate) {
-            car->y = car->verticalTargetY;
-        } else {
-            car->verticalMotionState = CAR_VERTICAL_FALLING;
-            car->verticalMotionRate = car->verticalMotionTimer;
-            car->y = car->verticalTargetY;
-        }
-    } else {
-        s16 fall = tick - (u16)car->verticalMotionRate;
-
-        car->y = car->verticalTargetY +
-                 fall * fall * CAR_JUMP_FALL_CURVE / CAR_JUMP_CURVE_SCALE;
-        if (car->y >= ground) {
-            car->verticalMotionState = CAR_VERTICAL_GROUNDED;
-        }
+    switch (car->verticalMotionState) {
+    case CAR_VERTICAL_RISING:
+        UpdateRivalRise(car, ground, tick);
+        break;
+    case CAR_VERTICAL_AT_CREST:
+        UpdateRivalCrest(car, ground);
+        break;
+    case CAR_VERTICAL_FALLING:
+    default:
+        UpdateRivalFall(car, ground, tick);
+        break;
     }
 
     if (car->verticalMotionState == CAR_VERTICAL_GROUNDED) {
-        car->y = ground + CAR_WHEEL_GROUND_OFFSET;
-        car->verticalPitch = 0;
-        car->verticalRoll = 0;
-        StartCarBodyKick(car, CAR_BODY_KICK_LANDING);
+        LandRivalCar(car, ground);
     }
 }
 
