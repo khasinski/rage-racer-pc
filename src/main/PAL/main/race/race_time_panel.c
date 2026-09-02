ca708b6fd12c5fefeeb7ffe799dc74d5ba61692c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-02 20:00:51 +0200

    Name race time panel layout

--- src/main/PAL/main/race/race_time_panel.c
+++ src/main/PAL/main/race/race_time_panel.c
@@ -6,7 +6,18 @@
 #include "game/screens.h"
 
 enum {
-    LABELED_RACE_TIME_CAPACITY = LAP_TIME_TEXT_CAPACITY + 2
+    LABELED_RACE_TIME_CAPACITY = LAP_TIME_TEXT_CAPACITY + 2,
+    PANEL_LABEL_X = 0x10,
+    PANEL_VALUE_LEFT_X = 0x14,
+    PANEL_VALUE_RIGHT_X = 0xB0,
+    TOTAL_TIME_LABEL_Y = 0x80,
+    TOTAL_TIME_VALUE_Y = 0x90,
+    LAP_TIME_LABEL_Y = 0xA4,
+    LAP_TIME_VALUE_Y = 0xB0,
+    LAP_TIME_ROW_HEIGHT = 0xC,
+    LAP_ROWS_PER_COLUMN = 3,
+    PANEL_TEXT_CLUT = 0x7812,
+    NEW_RECORD_TEXT_CLUT = 0x784C,
 };
 
 static void FormatLabeledRaceTime(
@@ -23,23 +34,28 @@ void DrawRaceTimePanel(s32 slideY) {
     s32 bestTimeColor;
     s32 lap;
 
-    DrawProportionalText(0x10, slideY + 0x80, g_CaptionTotalTime, 0x7812);
+    DrawProportionalText(PANEL_LABEL_X, slideY + TOTAL_TIME_LABEL_Y,
+                         g_CaptionTotalTime, PANEL_TEXT_CLUT);
 
     FormatLabeledRaceTime(text, 'T', g_RaceTotalTime);
     bestTimeColor =
         g_BestTotalTimes[g_GrandPrixSeries][SeriesCourseIndex()]
                         [recordMode] == g_RaceTotalTime
-            ? 0x784C
-            : 0x7812;
-    DrawProportionalText(0x14, slideY + 0x90, text, bestTimeColor);
+            ? NEW_RECORD_TEXT_CLUT
+            : PANEL_TEXT_CLUT;
+    DrawProportionalText(PANEL_VALUE_LEFT_X, slideY + TOTAL_TIME_VALUE_Y,
+                         text, bestTimeColor);
 
-    DrawProportionalText(0x10, slideY + 0xA4, g_CaptionLapTime, 0x7812);
+    DrawProportionalText(PANEL_LABEL_X, slideY + LAP_TIME_LABEL_Y,
+                         g_CaptionLapTime, PANEL_TEXT_CLUT);
     for (lap = 0; lap < lapCount; lap++) {
-        s32 x = lap < 3 ? 0x14 : 0xB0;
-        s32 y = slideY + 0xB0 + (lap % 3) * 0xC;
+        s32 x = lap < LAP_ROWS_PER_COLUMN ? PANEL_VALUE_LEFT_X
+                                          : PANEL_VALUE_RIGHT_X;
+        s32 y = slideY + LAP_TIME_VALUE_Y +
+                (lap % LAP_ROWS_PER_COLUMN) * LAP_TIME_ROW_HEIGHT;
         s32 color = g_PlayerCar.drive.hudLapHighlightRow == lap
-                        ? 0x784C
-                        : 0x7812;
+                        ? NEW_RECORD_TEXT_CLUT
+                        : PANEL_TEXT_CLUT;
 
         FormatLabeledRaceTime(
             text, (char)('1' + lap),
