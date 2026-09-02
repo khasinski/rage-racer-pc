c5ce411f2e873c8906e5eddd3f6a2a057b0653d3
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-02 16:03:43 +0200

    Name BGM select transition state

--- src/main/PAL/main/race/bgm_select_transition.c
+++ src/main/PAL/main/race/bgm_select_transition.c
@@ -10,23 +10,32 @@
 enum {
     BGM_SELECT_FADE_TPAGE = 0x49,
     BGM_SELECT_OPAQUE_FADE = 257,
+    BGM_SELECT_FRAME_SYNC_THRESHOLD = 0x80,
+    BGM_SELECT_INITIAL_FADE = 0x13C,
+    BGM_SELECT_FADE_IN_STEP = -4,
+    BGM_SELECT_FADE_OUT_STEP = 4,
+    BGM_SELECT_SCENE_ID = 0x1C,
+    BGM_SELECT_DEFAULT_CURSOR = 1,
+    BGM_SELECT_INITIAL_TRACK = 0,
+    BGM_SELECT_INITIAL_CHANGE_DELAY = 30,
+    BGM_SELECT_DISPLAY_ENABLE_FRAME = 15,
     OPTION_SCENE_ID = 0x16,
 };
 
 void EnterBgmSelectScreen(void) {
     SetDispMask(0);
     SetupDisplay240(0, 0, 0);
-    g_FrameSyncThreshold = 0x80;
-    g_FadeLevel = 0x13C;
-    g_FadeStep = -4;
-    g_SceneId = 0x1C;
-    g_BgmSelectCursor = 1;
+    g_FrameSyncThreshold = BGM_SELECT_FRAME_SYNC_THRESHOLD;
+    g_FadeLevel = BGM_SELECT_INITIAL_FADE;
+    g_FadeStep = BGM_SELECT_FADE_IN_STEP;
+    g_SceneId = BGM_SELECT_SCENE_ID;
+    g_BgmSelectCursor = BGM_SELECT_DEFAULT_CURSOR;
     g_BgmSelectShowUi = 1;
-    g_BgmSelectCdTrack = 3;
+    g_BgmSelectCdTrack = BgmCdTrack(BGM_SELECT_INITIAL_TRACK);
     g_BgmSelectStep = BGM_SELECT_STEP_LOAD_ASSETS;
     g_SceneTimer = 0;
-    g_BgmSelectTrack = 0;
-    g_BgmChangeDelay = 0x1E;
+    g_BgmSelectTrack = BGM_SELECT_INITIAL_TRACK;
+    g_BgmChangeDelay = BGM_SELECT_INITIAL_CHANGE_DELAY;
     g_CdTrackEnded = 0;
     g_CameraCarIndex = 0;
 }
@@ -46,7 +55,7 @@ static s32 AdvanceBgmSelectFade(void) {
 }
 
 static void UpdateBgmSelectTransition(void) {
-    if (g_SceneTimer == 0xF) {
+    if (g_SceneTimer == BGM_SELECT_DISPLAY_ENABLE_FRAME) {
         SetDispMask(1);
     }
 
@@ -72,14 +81,14 @@ void UpdateBgmSelectLoad(void) {
 
 void UpdateBgmSelectFadeIn(void) {
     if (g_AssetLoadState == 0) {
-        g_FadeStep = 4;
+        g_FadeStep = BGM_SELECT_FADE_OUT_STEP;
     }
     UpdateBgmSelectTransition();
 }
 
 void ExitBgmSelect(void) {
     if (g_AssetLoadState == 0) {
-        g_FadeStep = 4;
+        g_FadeStep = BGM_SELECT_FADE_OUT_STEP;
     }
 
     if (AdvanceBgmSelectFade()) {
