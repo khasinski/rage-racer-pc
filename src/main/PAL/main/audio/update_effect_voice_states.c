f17d1cb4460ce9a01ebf97bf2dbf23c7d422b93d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-02 17:52:59 +0200

    Share effect voice key on

--- src/main/PAL/main/audio/update_effect_voice_states.c
+++ src/main/PAL/main/audio/update_effect_voice_states.c
@@ -8,17 +8,23 @@ enum {
     EFFECT_BASE_NOTE = 0x3C,
 };
 
-static void WriteEffectVoicePitch(s32 hardwareVoice, EffectVoice *effect) {
+static void KeyOnEffectVoice(s16 hardwareVoice, const EffectVoice *effect) {
+    SsUtKeyOnV(hardwareVoice, g_SoundScale.vabIds[0],
+               effect->note.half.value, (s16)effect->tone,
+               EFFECT_BASE_NOTE, 0, 0, 0);
+}
+
+static void WriteEffectVoicePitch(s16 hardwareVoice, EffectVoice *effect) {
     s32 volume = ClampVoiceVolume(
         effect->volume * g_SoundScale.scale / 128);
 
-    SsUtSetVVol((s16)hardwareVoice, volume, volume);
+    SsUtSetVVol(hardwareVoice, volume, volume);
     SsUtChangePitch(
-        (s16)hardwareVoice, 0, effect->note.half.value, EFFECT_BASE_NOTE, 0,
+        hardwareVoice, 0, effect->note.half.value, EFFECT_BASE_NOTE, 0,
         (s16)(effect->pitch.value >> 7), effect->pitch.half.fraction & 0x7F);
 }
 
-static void ApplyEffectVoicePitch(s32 hardwareVoice, EffectVoice *effect) {
+static void ApplyEffectVoicePitch(s16 hardwareVoice, EffectVoice *effect) {
     WriteEffectVoicePitch(hardwareVoice, effect);
     effect->state = EFFECT_VOICE_IDLE;
 }
@@ -28,15 +34,13 @@ void ForcePitchEffectVoicesEnabled(s32 enabled) {
 
     for (index = 0; index < EFFECT_VOICE_COUNT; index++) {
         EffectVoice *effect = &g_EffectVoices[index];
-        s32 hardwareVoice = EFFECT_HARDWARE_VOICE_FIRST + index;
+        s16 hardwareVoice = (s16)(EFFECT_HARDWARE_VOICE_FIRST + index);
 
         if (enabled != 0) {
-            SsUtKeyOnV((s16)hardwareVoice, g_SoundScale.vabIds[0],
-                       effect->note.half.value, (s16)effect->tone,
-                       EFFECT_BASE_NOTE, 0, 0, 0);
+            KeyOnEffectVoice(hardwareVoice, effect);
             WriteEffectVoicePitch(hardwareVoice, effect);
         } else {
-            SsUtKeyOffV((s16)hardwareVoice);
+            SsUtKeyOffV(hardwareVoice);
         }
     }
 }
@@ -46,20 +50,18 @@ void UpdateEffectVoiceStates(void) {
 
     for (index = 0; index < EFFECT_VOICE_COUNT; index++) {
         EffectVoice *effect = &g_EffectVoices[index];
-        s32 hardwareVoice = EFFECT_HARDWARE_VOICE_FIRST + index;
+        s16 hardwareVoice = (s16)(EFFECT_HARDWARE_VOICE_FIRST + index);
 
         switch (effect->state) {
         case EFFECT_VOICE_START:
-            SsUtKeyOnV((s16)hardwareVoice, g_SoundScale.vabIds[0],
-                       effect->note.half.value, (s16)effect->tone,
-                       EFFECT_BASE_NOTE, 0, 0, 0);
+            KeyOnEffectVoice(hardwareVoice, effect);
             ApplyEffectVoicePitch(hardwareVoice, effect);
             break;
         case EFFECT_VOICE_UPDATE:
             ApplyEffectVoicePitch(hardwareVoice, effect);
             break;
         case EFFECT_VOICE_STOP:
-            SsUtKeyOffV((s16)hardwareVoice);
+            SsUtKeyOffV(hardwareVoice);
             effect->state = EFFECT_VOICE_IDLE;
             break;
         case EFFECT_VOICE_IDLE:
