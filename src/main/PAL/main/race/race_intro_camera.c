952e0fa9423bcd110a74030b7fac5d4cf2294a35
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-02 04:20:00 +0200

    Test and clarify race intro camera

--- src/main/PAL/main/race/race_intro_camera.c
+++ src/main/PAL/main/race/race_intro_camera.c
@@ -4,14 +4,13 @@
 #include "game/render.h"
 #include "game/track.h"
 
+enum { INTRO_CAR_VIEW_HEIGHT = 28 };
+
 void RunRaceIntroCamera(PlayerCarRuntime *car, s32 mode) {
     GameViewWork viewWork;
-    GameViewWork *view;
-    s32 carViewHeight = 28;
     s32 delta[3];
 
     LoadViewWork(&viewWork);
-    view = &viewWork;
 
     if (mode >= 90) {
         UpdateCamera(CAMERA_VIEW_CAR, (GameRenderObject *)car);
@@ -20,7 +19,8 @@ void RunRaceIntroCamera(PlayerCarRuntime *car, s32 mode) {
 
     if (mode < 2) {
         RaceIntroCameraScript *script = g_RaceIntroCameraScript;
-        s16 keyIndex = script->firstKeyIndex[ReadStableRaceSeries()];
+        s32 series = ReadStableRaceSeries() != 0;
+        s16 keyIndex = script->firstKeyIndex[series];
         RaceIntroCameraKey *key = &script->keys[keyIndex];
 
         g_RaceIntroCameraCursor = key;
@@ -41,7 +41,8 @@ void RunRaceIntroCamera(PlayerCarRuntime *car, s32 mode) {
             if (key[1].mode == 1) {
                 g_RaceIntroCameraDelta.vx = (u16)car->x - key[1].x.half.value;
                 g_RaceIntroCameraDelta.vy =
-                    (u16)car->y - carViewHeight - key[1].y.half.value;
+                    (u16)car->y - INTRO_CAR_VIEW_HEIGHT -
+                    key[1].y.half.value;
                 g_RaceIntroCameraDelta.vz = (u16)car->z - key[1].z.half.value;
             } else {
                 g_RaceIntroCameraDelta.vx =
@@ -60,39 +61,41 @@ void RunRaceIntroCamera(PlayerCarRuntime *car, s32 mode) {
     }
 
     if (g_RaceIntroCameraCursor->mode == 0) {
-        s32 interpolationAngle =
-            (g_RaceIntroCameraTimer << 10) /
-            g_RaceIntroCameraCursor->duration;
+        s32 duration = g_RaceIntroCameraCursor->duration;
+        s32 interpolationAngle = duration > 0
+                                     ? (g_RaceIntroCameraTimer << 10) / duration
+                                     : 0;
         s32 interpolation = rcos(interpolationAngle);
 
-        view->x = g_RaceIntroCameraCursor->x.word +
-                  g_RaceIntroCameraDelta.vx * interpolation / 4096;
-        view->y = g_RaceIntroCameraCursor->y.word +
-                  g_RaceIntroCameraDelta.vy * interpolation / 4096;
-        view->z = g_RaceIntroCameraCursor->z.word +
-                  g_RaceIntroCameraDelta.vz * interpolation / 4096;
+        viewWork.x = g_RaceIntroCameraCursor->x.word +
+                     g_RaceIntroCameraDelta.vx * interpolation / 4096;
+        viewWork.y = g_RaceIntroCameraCursor->y.word +
+                     g_RaceIntroCameraDelta.vy * interpolation / 4096;
+        viewWork.z = g_RaceIntroCameraCursor->z.word +
+                     g_RaceIntroCameraDelta.vz * interpolation / 4096;
 
-        delta[0] = rsin(car->bodyYaw) / 128 + car->x - view->x;
-        delta[1] = car->y - carViewHeight - view->y;
-        delta[2] = rcos(car->bodyYaw) / 128 + car->z - view->z;
-        view->angleY = ANGLE_QUARTER_TURN - Atan2(delta[0], delta[2]);
-        view->angleX = ANGLE_QUARTER_TURN -
-                       Atan2(delta[1], DistanceXZ(delta[0], delta[2]) >> 6);
-        view->angleZ = 0;
+        delta[0] = rsin(car->bodyYaw) / 128 + car->x - viewWork.x;
+        delta[1] = car->y - INTRO_CAR_VIEW_HEIGHT - viewWork.y;
+        delta[2] = rcos(car->bodyYaw) / 128 + car->z - viewWork.z;
+        viewWork.angleY = ANGLE_QUARTER_TURN - Atan2(delta[0], delta[2]);
+        viewWork.angleX = ANGLE_QUARTER_TURN -
+                          Atan2(delta[1],
+                                DistanceXZ(delta[0], delta[2]) >> 6);
+        viewWork.angleZ = 0;
         StoreViewWork(&viewWork);
         SetCameraRotMatrix();
         SelectModelBank(0);
         DrawPlayerCarModel((GameRenderObject *)car);
     } else {
         DrawFullscreenFadeTile(g_RaceIntroCameraTimer * 26, 0x29);
-        view->x = car->x;
-        view->y = car->y - carViewHeight;
-        view->z = car->z;
-        view->reserved = car->positionW;
-        view->angleX = car->bodyPitch;
-        view->angleY = car->bodyYaw;
-        view->angleZ = car->bodyRoll;
-        view->depth = car->bodyRotationW;
+        viewWork.x = car->x;
+        viewWork.y = car->y - INTRO_CAR_VIEW_HEIGHT;
+        viewWork.z = car->z;
+        viewWork.reserved = car->positionW;
+        viewWork.angleX = car->bodyPitch;
+        viewWork.angleY = car->bodyYaw;
+        viewWork.angleZ = car->bodyRoll;
+        viewWork.depth = car->bodyRotationW;
         StoreViewWork(&viewWork);
         SetCameraRotMatrix();
     }
---
+++ tests/car/race_intro_camera_tests.c
@@ -0,0 +1,128 @@
+#include "common.h"
+#include "game/car.h"
+#include "game/car_internal.h"
+#include "game/race.h"
+#include "game/render.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+GameRenderState g_RenderState;
+RaceIntroCameraScript *g_RaceIntroCameraScript;
+RaceIntroCameraKey *g_RaceIntroCameraCursor;
+SVec g_RaceIntroCameraDelta;
+s32 g_RaceIntroCameraTimer;
+s32 g_RaceSeries;
+
+static int s_atanCalls;
+static int s_matrixCalls;
+static int s_drawCalls;
+static int s_updateCameraCalls;
+static int s_fadeCalls;
+static s32 s_fadeColor;
+static s32 s_selectedBank;
+
+s32 Atan2(s32 x, s32 y) {
+    (void)x;
+    (void)y;
+    return ++s_atanCalls * 100;
+}
+
+void SetCameraRotMatrix(void) { s_matrixCalls++; }
+
+void SelectModelBank(s32 index) { s_selectedBank = index; }
+
+void DrawPlayerCarModel(GameRenderObject *car) {
+    (void)car;
+    s_drawCalls++;
+}
+
+void DrawFullscreenFadeTile(s32 color, s32 tpage) {
+    (void)tpage;
+    s_fadeCalls++;
+    s_fadeColor = color;
+}
+
+void UpdateCamera(CameraViewMode mode, GameRenderObject *car) {
+    (void)car;
+    if (mode == CAMERA_VIEW_CAR) {
+        s_updateCameraCalls++;
+    }
+}
+
+#define CHECK_EQ(actual, expected) do {                                        \
+    if ((actual) != (expected)) {                                               \
+        fprintf(stderr, "line %d: %s = %lld, expected %lld\n", __LINE__,       \
+                #actual, (long long)(intptr_t)(actual),                         \
+                (long long)(intptr_t)(expected));                               \
+        return 1;                                                               \
+    }                                                                           \
+} while (0)
+
+int main(void) {
+    static struct {
+        s16 firstKeyIndex[2];
+        RaceIntroCameraKey keys[3];
+    } script;
+    PlayerCarRuntime car;
+    s32 interpolation;
+
+    memset(&script, 0, sizeof(script));
+    memset(&car, 0, sizeof(car));
+    memset(&g_RenderState, 0, sizeof(g_RenderState));
+    g_RaceIntroCameraScript = (RaceIntroCameraScript *)(void *)&script;
+    script.firstKeyIndex[0] = 0;
+    script.firstKeyIndex[1] = 1;
+    script.keys[0].x.half.value = 100;
+    script.keys[0].y.half.value = 200;
+    script.keys[0].z.half.value = 300;
+    script.keys[0].duration = 4;
+    script.keys[0].startFrame = 2;
+    script.keys[1].x.half.value = 500;
+    script.keys[1].y.half.value = 600;
+    script.keys[1].z.half.value = 700;
+    script.keys[1].mode = 1;
+    script.keys[1].duration = 3;
+    car.x = 1000;
+    car.y = 1100;
+    car.z = 1200;
+
+    RunRaceIntroCamera(&car, 0);
+    interpolation = rcos(3 << 8);
+    CHECK_EQ(g_RaceIntroCameraCursor, &script.keys[0]);
+    CHECK_EQ(g_RaceIntroCameraTimer, 3);
+    CHECK_EQ(g_RenderState.viewX, 100 + 400 * interpolation / 4096);
+    CHECK_EQ(g_RenderState.viewY, 200 + 400 * interpolation / 4096);
+    CHECK_EQ(g_RenderState.viewZ, 300 + 400 * interpolation / 4096);
+    CHECK_EQ(g_RenderState.viewAngleY, ANGLE_QUARTER_TURN - 100);
+    CHECK_EQ(g_RenderState.viewAngleX, ANGLE_QUARTER_TURN - 200);
+    CHECK_EQ(s_drawCalls, 1);
+    CHECK_EQ(s_selectedBank, 0);
+
+    RunRaceIntroCamera(&car, 2);
+    CHECK_EQ(g_RaceIntroCameraCursor, &script.keys[1]);
+    CHECK_EQ(g_RaceIntroCameraTimer, 2);
+    CHECK_EQ(g_RaceIntroCameraDelta.vx, 500);
+    CHECK_EQ(g_RaceIntroCameraDelta.vy, 472);
+    CHECK_EQ(g_RaceIntroCameraDelta.vz, 500);
+    CHECK_EQ(g_RenderState.viewX, car.x);
+    CHECK_EQ(g_RenderState.viewY, car.y - 28);
+    CHECK_EQ(g_RenderState.viewZ, car.z);
+    CHECK_EQ(s_fadeCalls, 1);
+    CHECK_EQ(s_fadeColor, 52);
+    CHECK_EQ(s_matrixCalls, 2);
+
+    RunRaceIntroCamera(&car, 90);
+    CHECK_EQ(s_updateCameraCalls, 1);
+    CHECK_EQ(g_RaceIntroCameraTimer, 2);
+
+    script.keys[0].duration = 0;
+    script.keys[0].mode = 0;
+    RunRaceIntroCamera(&car, 0);
+    CHECK_EQ(g_RaceIntroCameraTimer, 0);
+    CHECK_EQ(g_RenderState.viewX, script.keys[1].x.word);
+
+    puts("race intro camera preserves script, fade, and handoff modes");
+    return 0;
+}
