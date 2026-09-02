3ef7bbccc2ec441b170aa178dfa152c3f260bf4d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-02 15:27:48 +0200

    Keep result screen intros together

--- src/main/PAL/main/race/result_screen.c
+++ src/main/PAL/main/race/result_screen.c
@@ -26,6 +26,12 @@ void DrawResultScreen(void) {
         ot, next, 0, 0x30, 0x140, 0x18, 0xF0, 0xF0, 0xF0);
 }
 
+void DrawCourseIntro(void) {
+    DrawProportionalText(0x10, 0x1C, g_TextTimeAttack, 0x7812);
+    DrawText8x8Trans(0x10, 0x39, g_TextCourseIn, 0x78CC);
+    DrawResultScreen();
+}
+
 static void DrawClassPlaceBanner(void) {
     GameOrderingTableEntry *ot = GamePrimaryOrderingTable(0);
     const ResultPlaceBarPosition *bar =
