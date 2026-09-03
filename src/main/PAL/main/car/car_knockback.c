b8e96ccf048b4d9f040d304ca78a0ba228beedd5
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-03 20:13:28 +0200

    Name car knockback response scales

--- src/main/PAL/main/car/car_knockback.c
+++ src/main/PAL/main/car/car_knockback.c
@@ -13,6 +13,14 @@ enum {
     TRACK_BOUNDARY_KNOCKBACK_DURATION = 0x1E,
     CAR_COLLISION_KNOCKBACK_DURATION = 0x0F,
     FIXED_TRACK_KNOCKBACK_FIRST_MODE = CAR_TRACK_CONTACT_FRONT_RIGHT,
+    LOW_SPEED_STRENGTH_SCALE = 50,
+    KNOCKBACK_BASE_STRENGTH = 10,
+    FIXED_TRIG_SCALE = 4096,
+    HIGH_SPEED_STRENGTH_DIVISOR = 65536,
+    SUPPLIED_VECTOR_DIVISOR = 2,
+    KNOCKBACK_DECAY_NUMERATOR = 7,
+    KNOCKBACK_DECAY_DENOMINATOR = 8,
+    FIXED_TRACK_KNOCKBACK_STRENGTH = 20,
 };
 
 static s32 TrackOutwardAngle(const GameCarRuntime *car) {
@@ -38,28 +46,32 @@ static s32 TrackBoundaryPushStrength(const GameCarRuntime *car) {
     s32 speed;
 
     if (car->speed < LOW_SPEED_KNOCKBACK_THRESHOLD) {
-        return (sine * 50) / 4096 + 10;
+        return (sine * LOW_SPEED_STRENGTH_SCALE) / FIXED_TRIG_SCALE +
+               KNOCKBACK_BASE_STRENGTH;
     }
 
     speed = car->speed <= MAX_SPEED_SCALED_KNOCKBACK
         ? car->speed
         : MAX_SPEED_SCALED_KNOCKBACK;
-    return (speed * sine) / 65536 + 10;
+    return (speed * sine) / HIGH_SPEED_STRENGTH_DIVISOR +
+           KNOCKBACK_BASE_STRENGTH;
 }
 
 static void SetKnockbackVector(GameCarRuntime *car, s32 angle, s32 strength,
                                u16 duration) {
     car->motionActive = 1;
     car->motionTimer = duration;
-    car->velocityX = WrapSigned16((rsin(angle) * strength) / 4096);
-    car->velocityZ = WrapSigned16((rcos(angle) * strength) / 4096);
+    car->velocityX = WrapSigned16(
+        (rsin(angle) * strength) / FIXED_TRIG_SCALE);
+    car->velocityZ = WrapSigned16(
+        (rcos(angle) * strength) / FIXED_TRIG_SCALE);
 }
 
 static void SetSuppliedKnockbackVector(GameCarRuntime *car, s32 x, s32 z) {
     car->motionActive = 1;
     car->motionTimer = CAR_COLLISION_KNOCKBACK_DURATION;
-    car->velocityX = WrapSigned16(x / 2);
-    car->velocityZ = WrapSigned16(z / 2);
+    car->velocityX = WrapSigned16(x / SUPPLIED_VECTOR_DIVISOR);
+    car->velocityZ = WrapSigned16(z / SUPPLIED_VECTOR_DIVISOR);
 }
 
 static void TraceCarKnockback(GameCarRuntime *car, s32 inputX, s32 inputZ,
@@ -104,8 +116,10 @@ void ApplyCarKnockback(GameCarRuntime *car) {
 
     car->x = WrapSigned32((int64_t)car->x - car->velocityX);
     car->z = WrapSigned32((int64_t)car->z - car->velocityZ);
-    car->velocityX = car->velocityX * 7 / 8;
-    car->velocityZ = car->velocityZ * 7 / 8;
+    car->velocityX = car->velocityX * KNOCKBACK_DECAY_NUMERATOR /
+                     KNOCKBACK_DECAY_DENOMINATOR;
+    car->velocityZ = car->velocityZ * KNOCKBACK_DECAY_NUMERATOR /
+                     KNOCKBACK_DECAY_DENOMINATOR;
 }
 
 void SetTrackBoundaryKnockback(GameCarRuntime *car, s32 x, s32 z,
@@ -118,7 +132,8 @@ void SetTrackBoundaryKnockback(GameCarRuntime *car, s32 x, s32 z,
                            TrackBoundaryPushStrength(car),
                            TRACK_BOUNDARY_KNOCKBACK_DURATION);
     } else if (contact < CAR_TRACK_CONTACT_REAR_RIGHT) {
-        SetKnockbackVector(car, TrackBoundaryPushAngle(car), 20,
+        SetKnockbackVector(car, TrackBoundaryPushAngle(car),
+                           FIXED_TRACK_KNOCKBACK_STRENGTH,
                            CAR_COLLISION_KNOCKBACK_DURATION);
     } else {
         SetSuppliedKnockbackVector(car, x, z);
