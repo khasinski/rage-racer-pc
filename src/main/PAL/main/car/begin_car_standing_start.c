5e7ad83f359bf414a029e577690a3c1e3c2051c4
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-03 15:30:10 +0200

    Name standing start setup parameters

--- src/main/PAL/main/car/begin_car_standing_start.c
+++ src/main/PAL/main/car/begin_car_standing_start.c
@@ -1,6 +1,18 @@
 #include "game/car.h"
 #include "game/integer.h"
 
+enum {
+    FIRST_FORWARD_GEAR = 1,
+    MINIMUM_REV_LIMIT = 1,
+    STANDING_START_SPIN_SCALE = 10000,
+    LOW_RPM_SPIN_CUTOFF = 2000,
+    LOW_RPM_SPIN_OFFSET = 1000,
+    OUTPUT_LOAD_PER_GEAR = 200,
+    OUTPUT_LOAD_BASE = 300,
+    GRIP_LOSS_FIRST_GEAR = 2,
+    GRIP_LOSS_FRAMES = 200,
+};
+
 /* Seeds launch spin from revs above the power peak. Starting in second gear
  * or higher also begins with a short loss of grip. */
 void BeginCarStandingStart(PlayerCarRuntime *car) {
@@ -8,30 +20,34 @@ void BeginCarStandingStart(PlayerCarRuntime *car) {
     s32 gear = car->drive.gear;
     s32 revLimit = g_CarSpec->revLimit;
 
-    if (gear < 1) {
-        gear = 1;
+    if (gear < FIRST_FORWARD_GEAR) {
+        gear = FIRST_FORWARD_GEAR;
     } else if (gear > CAR_FORWARD_GEAR_COUNT) {
         gear = CAR_FORWARD_GEAR_COUNT;
     }
     car->drive.gear = (s16)gear;
     if (revLimit <= 0) {
-        revLimit = 1;
+        revLimit = MINIMUM_REV_LIMIT;
     }
 
     spin = WrapSigned32(
         (int64_t)g_EngineRpm - g_PeakOutputRpm);
-    spin = WrapSigned32((int64_t)spin * 10000) / revLimit;
+    spin = WrapSigned32(
+        (int64_t)spin * STANDING_START_SPIN_SCALE) / revLimit;
     g_GripLossTimer = 0;
 
     if (spin < 0) {
-        spin = g_EngineRpm < 2000 ? 0 : g_EngineRpm - 1000;
+        spin = g_EngineRpm < LOW_RPM_SPIN_CUTOFF
+                   ? 0
+                   : g_EngineRpm - LOW_RPM_SPIN_OFFSET;
     } else {
         spin = WrapSigned32(
             (int64_t)spin *
-            (g_PeakOutputValue / ((gear * 200) + 300)));
+            (g_PeakOutputValue /
+             (gear * OUTPUT_LOAD_PER_GEAR + OUTPUT_LOAD_BASE)));
         car->drive.drivetrainTorque /= gear;
-        if (gear >= 2) {
-            g_GripLossTimer = 200;
+        if (gear >= GRIP_LOSS_FIRST_GEAR) {
+            g_GripLossTimer = GRIP_LOSS_FRAMES;
         }
     }
 
